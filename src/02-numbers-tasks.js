6cba253e6cde5a1ab97cb30b0571ebdbad472ce1
Eugeku <eugenekuzora@gmail.com>
2024-09-23 18:20:56 +0300

    feat: implement all number functions

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -206,8 +206,14 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(n); i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -225,8 +231,9 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const converted = Number(value);
+  return Number.isNaN(converted) ? def : converted;
 }
 
 module.exports = {
